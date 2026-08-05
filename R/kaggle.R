9d262871ecba5202603fe2cf53c6bce9601c365b
Felipe <efelipecarlos@gmail.com>
2026-08-05 15:47:37 -0300

    Update package installation method in kaggle.R

--- R/kaggle.R
+++ R/kaggle.R
@@ -59,7 +59,7 @@ install <- function(output_dir) {
     # Install terra
     pak::pkg_install("terra@1.9-27")
     # Remove torch
-    devtools::install_cran(packages_to_install[["package"]])
+    purrr::map(packages_to_install[["package"]], pak::pak)
     # Install sits
     pak::pak("e-sensing/sits@dev")
     # Create directory to the bundle
